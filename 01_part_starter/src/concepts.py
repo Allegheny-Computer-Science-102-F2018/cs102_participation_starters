39ec379ebdaf6bfe808044f69b5791b796fb9e44
oliver bonhamcarter <obonhamcarter@allegheny.edu>
2018-10-23 08:51:00 -0400

    participation starters

---
+++ 01_part_starter/src/concepts.py
@@ -0,0 +1,17 @@
+## Date: 22 October 2018
+## Name: NAME
+
+# PYTHON CODE
+
+# First problem to Discuss
+# What problem is it?
+
+
+# Example of this concept in use with comments to provide an understaning of what is happening in the code. 
+
+
+# Second problem to Discuss
+# What problem is it?
+
+# Example of this concept in use with comments to provide an understaning of what is happening in the code. 
+
---
+++ 01_part_starter/writing/concepts.md
@@ -0,0 +1,21 @@
+## Date: 22 October 2018
+## Name: NAME
+
+
+# First problem to discuss
+# 1a) What problem is it?
+
+# 2a) Discussion of the concepts in the problem
+
+# 3a) Why is this concept important in computer science?
+
+
+
+
+# Second problem to discuss
+# 1b) What problem is it?
+
+# 2b) Discussion of the concepts in the problem
+
+# 3b) Why is this concept important in computer science?
+
